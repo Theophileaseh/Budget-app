821dea6b4391afaf3f3c9731247d2f49407ea277
Theophileaseh <theophilenwachan@gmail.com>
2022-09-08 18:29:23 +0100

    Update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,13 +92,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   
+  config.action_mailer.default_url_options = {:host => 'localhost:8080'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    port: 587,
-    address: 'smtp.gmail.com',
-    user_name: ENV['SMTP_USER_NAME'],
-    password: ENV['SMTP_PASSWORD'],
-    authentication: :plain,
-    enable_starttls_auto: true
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => "testvagabond@gmail.com",
+   :password             => "testvagabon",
+   :authentication       => "plain",
+  :enable_starttls_auto => true
   }
 end
